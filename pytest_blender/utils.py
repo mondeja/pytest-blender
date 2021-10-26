f222a261a9990a21757a906eac7692921ef28a06
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2021-10-26 17:26:01 +0200

    Improve Blender executable path discovering in MacOS

--- pytest_blender/utils.py
+++ pytest_blender/utils.py
@@ -4,6 +4,8 @@ import sys
 
 def which_blender_by_os():
     """Get the expected Blender executable location by operative system."""
-    if sys.platform == "darwin":
-        return "Blender"
-    return "blender.exe" if "win" in sys.platform else shutil.which("blender")
+    return (
+        shutil.which("Blender")
+        if sys.platform == "darwin"
+        else ("blender.exe" if "win" in sys.platform else shutil.which("blender"))
+    )
