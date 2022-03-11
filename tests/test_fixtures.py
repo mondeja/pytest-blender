94c43f6da15338482f8302b72c961fe3c8b85f50
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-03-11 13:36:15 +0100

    Fix tests

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import shutil
 import subprocess
 
 import pytest
@@ -18,8 +19,13 @@ except ImportError:
     reason="Requires testing loading the pytest-blender plugin.",
 )
 def test_blender_executable(blender_executable):
-    assert os.path.isfile(blender_executable)
-    assert "blender" in os.path.basename(blender_executable).lower()
+    assert blender_executable
+    blender_executable_path = shutil.which(blender_executable)
+    if not os.path.isfile(blender_executable_path):
+        assert os.path.islink(blender_executable_path)
+    else:
+        assert os.path.isfile(blender_executable_path)
+    assert "blender" in os.path.basename(blender_executable_path).lower()
 
 
 @pytest.mark.skipif(
@@ -27,7 +33,11 @@ def test_blender_executable(blender_executable):
     reason="Requires testing loading the pytest-blender plugin.",
 )
 def test_blender_python_executable(blender_python_executable):
-    assert os.path.isfile(blender_python_executable)
+    assert blender_python_executable
+    if not os.path.isfile(blender_python_executable):
+        assert os.path.islink(blender_python_executable)
+    else:
+        assert os.path.isfile(blender_python_executable)
     assert "python" in os.path.basename(blender_python_executable)
 
 
