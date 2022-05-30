c99d24036ec96bd38a7fc186c271ca1775ce29bd
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-05-30 09:53:32 +0200

    Fix uncaught exception enabling invalid addons

--- tests/test_functionality_fixtures.py
+++ tests/test_functionality_fixtures.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from testing_utils import (
     ADDONS_DIRS,
@@ -43,3 +44,56 @@ def install_addons(install_addons_from_dir, uninstall_addons):
 
     assert "pytest_blender_zipped" not in os.listdir(BLENDER_USER_ADDONS_DIR)
     assert "pytest_blender_zipped_from_dir" not in os.listdir(BLENDER_USER_ADDONS_DIR)
+
+
+def test_install_addons_from_dir_invalid_addon(testing_context):
+    """When an invalid addon is found in the addons directory, must be
+    ignored.
+
+    An addon could be invalid if:
+
+    - It hasn't a `bl_info` attribute.
+    """
+    clean_addons()
+
+    with testing_context(
+        empty_inicfg=True,
+        files={
+            "conftest.py": """
+import os
+import pytest
+
+@pytest.fixture(scope="session", autouse=True)
+def install_addons(install_addons_from_dir, uninstall_addons):
+    addons_ids = install_addons_from_dir(
+        os.getcwd(),
+        addons_ids=['pytest_blender_zipped', 'helpers'],
+    )
+    yield
+    uninstall_addons(addons_ids)
+""",
+            "tests/test_addon_installed.py": render_addon_installed_test(
+                "pytest_blender_zipped"
+            ),
+            "tests/test_addon_not_installed.py": render_addon_not_installed_test(
+                "helpers"
+            ),
+        },
+    ) as ctx:
+        shutil.copyfile(
+            os.path.join(FOO_ADDONS_DIR, "pytest_blender_zipped.zip"),
+            os.path.join(ctx.rootdir, "pytest_blender_zipped.zip"),
+        )
+        with open(os.path.join(ctx.rootdir, "helpers.py"), "w") as f:
+            f.write('import sys\nsys.stdout.write("HELPERS")\n')
+
+        stdout, stderr, exitcode = ctx.run()
+        assert exitcode == 0, f"{stdout}\n{stderr}\n"
+        assert (
+            "[pytest-blender]"
+            " Skipping installation of module 'helpers' as is not a"
+            " Blender addon (missing 'bl_info' module attribute)"
+        ) == stderr
+
+    assert "pytest_blender_zipped" not in os.listdir(BLENDER_USER_ADDONS_DIR)
+    assert "helpers.py" not in BLENDER_USER_ADDONS_DIR
